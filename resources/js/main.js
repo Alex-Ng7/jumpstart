c2cd3e3f72a41859b155bc3619457300dae2b39b
Alex-Ng7 <alex.ng371@gmail.com>
2017-07-15 17:49:14 -0400

    Add files via upload
    first upload, don't know why the carousel isn't working

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 4000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    draggable: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+)};
