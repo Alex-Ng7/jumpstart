b813a4d824debb8517a6448fb327e738e222fded
Alex-Ng7 <alex.ng371@gmail.com>
2017-07-15 19:19:04 -0400

    Add files via upload
    had to change the slick js to be local since the cdn wasn't working, carousel and top nav animation works

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,4 +9,18 @@ $(document).ready(function(){
     slidesToShow: 1,
     slidesToScroll: 1
   });
-)};
+
+
+ var scrollDistance = 0;
+  $(window).scroll(function(){
+    var topOfPage = $(this).scrollTop();
+    if (topOfPage - scrollDistance > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrollDistance = topOfPage;
+    } else if (scrollDistance - topOfPage > 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollDistance = topOfPage;
+    }
+  });
+});
